adc7c02714d962688abbabec39270178c35b8ae2
sshurkhovetskii <sshurkhovetskii@gmail.com>
2024-03-26 09:41:49 +0200

    fix js

--- src/js/modules/mouseover-event.js
+++ src/js/modules/mouseover-event.js
@@ -1,13 +1,17 @@
-function mousEvent () {
-    const mobLists = document.querySelectorAll('.mob-list');
-    for (const mobList of mobLists) {
-        mobList.addEventListener('mouseover', () => {
-            mobList.style.color = '#F2BE22';
-        });
-        
-        mobList.addEventListener('mouseout', () => {
-            mobList.style.color = '';
-        });
-    }
+function mousEvent() {
+	document.addEventListener('DOMContentLoaded', () => {
+		const mobLists = document.querySelectorAll('.mob-list')
+		for (const mobList of mobLists) {
+			mobList.addEventListener('mouseover', () => {
+				// mobList.style.color = '#F2BE22'
+				mobList.classList.add('yellow')
+			})
+
+			mobList.addEventListener('mouseout', () => {
+				// mobList.style.color = ''
+				mobList.classList.remove('yellow')
+			})
+		}
+	})
 }
-export default mousEvent;
+export default mousEvent
